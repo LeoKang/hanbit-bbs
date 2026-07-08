decdb2d14fc50dc0c11dd5e1f493073daf8750f9
seungjin <xeungjin@nate.com>
2026-07-08 16:20:04 +0900

    DB setting

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE member
+(
+    id       INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(128) NOT NULL,
+    email    VARCHAR(256) NOT NULL UNIQUE,
+    password VARCHAR(256)
+);
+
+CREATE TABLE authority
+(
+    id        INTEGER AUTO_INCREMENT PRIMARY KEY,
+    authority VARCHAR(256),
+    member_id INTEGER,
+    FOREIGN KEY (member_id) REFERENCES MEMBER (id)
+);
+
+CREATE TABLE article
+(
+    id          INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(256),
+    description VARCHAR(4096),
+    created     TIMESTAMP,
+    updated     TIMESTAMP,
+    member_id   INTEGER,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
